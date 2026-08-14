670c6f79e9beaa02e10a23ee42169180885594fd
sulabh kumar sharma <sulabhsharma18@sulabhs-MacBook-Air.local>
2026-08-14 22:34:03 +0530

    update api main.py

--- src/api/main.py
+++ src/api/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from src.api.routes import pipeline, health
 
 app = FastAPI(title="AI Intelligence Pipeline API")
+from fastapi.middleware.cors import CORSMiddleware
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # include routes
 app.include_router(health.router)
 app.include_router(pipeline.router)
